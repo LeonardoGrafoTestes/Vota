aa2beda062bade3d206e6a3497ed25645c022187
LeonardoGrafoTestes <grafoteste@gmail.com>
2025-09-19 12:03:42 -0300

    Update Vota.py

--- Vota.py
+++ Vota.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import psycopg2
-import pandas as pd
-from datetime import datetime, timedelta
 
-# ------------------ CONFIGURAÇÕES ------------------
-MIN_VOTOS = 2        # quantidade mínima de votos para liberar resultados
-TEMPO_MINUTOS = 10   # tempo mínimo em minutos para liberar resultados após início
-
-# ------------------ CONEXÃO ------------------
 def get_connection():
     """Mantém a conexão ativa durante a sessão"""
     if "conn" not in st.session_state:
@@ -15,7 +8,7 @@ def get_connection():
             supabase = st.secrets["connections"]["supabase"]
             st.session_state["conn"] = psycopg2.connect(
                 host=supabase["host"],
-                port=supabase["port"],
+                port=int(supabase["port"]),
                 dbname=supabase["dbname"],
                 user=supabase["user"],
                 password=supabase["password"]
@@ -25,6 +18,3 @@ def get_connection():
             st.error(f"❌ Erro ao conectar ao banco: {e}")
             return None
     return st.session_state["conn"]
-    print("✅ Conexão OK!")
-except Exception as e:
-    print("❌ Erro:", e)
